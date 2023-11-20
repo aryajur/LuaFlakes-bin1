d6b819318c1c3a19fd80d6dcfac3390748850cfa
Milind Gupta <milind.gupta@gmail.com>
2023-11-20 09:01:55 -0800

    Added Lua index as well

--- index.lua
+++ index.lua
@@ -1,7 +1,12 @@
 -- Flakes file list
 
 return {
-	"all/all/tableUtils",
-	"Windows/x32/LuaSec",
-	"Windows/x32/luasocket",
+	modules = {
+		"all/all/tableUtils",
+		"Windows/x32/LuaSec",
+		"Windows/x32/luasocket",
+	},
+	lua = {
+		"Windows/x32/Lua/5.3"
+	}
 }
